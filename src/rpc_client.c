727ba59e3e1ae8348a8c3d7d7ebd1b3ad4c57b42
Jakub Klama <jakub.klama@gmail.com>
2017-05-07 23:40:38 +0200

    Create designated API for instantiating RPC clients.

---
+++ src/rpc_client.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015-2017 Two Pore Guys, Inc.
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted providing that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <rpc/client.h>
+#include <glib.h>
+#include "internal.h"
+
+static void *
+rpc_client_worker(void *arg)
+{
+	rpc_client_t client = arg;
+
+	g_main_loop_run(client->rci_g_loop);
+	return (NULL);
+}
+
+
+rpc_client_t
+rpc_client_create(const char *uri, int flags)
+{
+	rpc_client_t client;
+
+	client = g_malloc0(sizeof(*client));
+	client->rci_connection = rpc_connection_create(uri, flags);
+	client->rci_g_context = g_main_context_new();
+	client->rci_g_loop = g_main_loop_new(client->rci_g_context, false);
+	client->rci_thread = g_thread_new("librpc client", rpc_client_worker,
+	    client);
+
+	return (client);
+}
+
+rpc_connection_t
+rpc_client_get_connection(rpc_client_t client)
+{
+
+	return (client->rci_connection);
+}
+
+void
+rpc_client_close(rpc_client_t client)
+{
+
+}
