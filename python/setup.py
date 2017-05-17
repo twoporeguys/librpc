426e17f4157c7e3a66cd6059bcd215af3b3a282e
Wojciech Kloska <shangteus@gmail.com>
2017-05-17 20:22:14 +0200

    FIXED: make sure to link librpc in py-librpc setup.py

--- python/setup.py
+++ python/setup.py
@@ -40,9 +40,10 @@ setup(
         Extension(
             "librpc",
             ["librpc.pyx"],
+            extra_compile_args=["-fblocks"],
             cython_include_dirs=["./pxd"],
             include_dirs=["./../include"],
-            extra_link_args=["-g"],
+            extra_link_args=["-g", "-lrpc"],
         )
     ]
 )
