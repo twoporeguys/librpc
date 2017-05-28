36fc0e8365173be19ef493ba52bf619156c53405
Wojciech Kloska <shangteus@gmail.com>
2017-05-28 13:54:41 +0200

    UPDATED: store errors as rpc_error_t to be able to free GError instances in place

--- src/rpc_object.c
+++ src/rpc_object.c
@@ -38,7 +38,7 @@
 #include <rpc/object.h>
 #include "internal.h"
 
-GPrivate rpc_last_error = G_PRIVATE_INIT((GDestroyNotify)g_error_free);
+GPrivate rpc_last_error = G_PRIVATE_INIT((GDestroyNotify)g_free);
 
 static const char *rpc_types[] = {
     [RPC_TYPE_NULL] = "null",
@@ -193,21 +193,22 @@ rpc_create_description(GString *description, rpc_object_t object,
 }
 
 void
-rpc_set_last_error(GError *error) {
+rpc_set_last_error(GError *g_error) {
+	rpc_error_t error;
+
+	error = g_malloc(sizeof(rpc_error_t));
+	error->code = g_error->code;
+	error->message = g_strdup(g_error->message);
+
 	g_private_replace(&rpc_last_error, error);
 }
 
 rpc_error_t
 rpc_get_last_error(void) {
-	GError *g_error;
 	rpc_error_t error;
 
-	if ((g_error = g_private_get(&rpc_last_error)) == NULL)
+	if ((error = g_private_get(&rpc_last_error)) == NULL)
 		return (NULL);
-
-	error = g_malloc(sizeof(rpc_error_t));
-	error->code = g_error->code;
-	error->message = g_strdup(g_error->message);
 
 	return error;
 }
