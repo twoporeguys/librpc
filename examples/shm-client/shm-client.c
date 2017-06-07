ea9dd9901d3fc212a97c173e9da68459f4fb97df
Jakub Klama <jakub.klama@gmail.com>
2017-06-08 00:04:15 +0200

    Add shared memory example apps.

---
+++ examples/shm-client/shm-client.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017 Two Pore Guys, Inc.
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted providing that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <glib.h>
+#include <rpc/object.h>
+#include <rpc/client.h>
+#include <rpc/shmem.h>
+
+#define	BLOCK_SIZE	(1024 * 1024)
+
+int
+main(int argc, const char *argv[])
+{
+	rpc_client_t client;
+	rpc_connection_t conn;
+	rpc_object_t result;
+	rpc_shmem_block_t b1, b2;
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: shm-client <server socket URI>\n");
+		return (1);
+	}
+
+	client = rpc_client_create(argv[1], 0);
+	if (client == NULL) {
+		fprintf(stderr, "cannot connect: %s\n", strerror(errno));
+		return (1);
+	}
+
+	conn = rpc_client_get_connection(client);
+	b1 = rpc_shmem_alloc(BLOCK_SIZE);
+	memset(rpc_shmem_block_get_ptr(b1), 'A', rpc_shmem_block_get_size(b1));
+
+	result = rpc_connection_call_sync(conn, "exchange_blob",
+	    rpc_shmem_create(b1), NULL);
+
+	printf("result = %s\n", rpc_copy_description(result));
+	b2 = rpc_shmem_get_block(result);
+
+	rpc_client_close(client);
+	return (0);
+}
---
+++ examples/shm-server/shm-server.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Two Pore Guys, Inc.
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted providing that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <rpc/object.h>
+#include <rpc/service.h>
+#include <rpc/server.h>
+#include <rpc/discovery.h>
+#include <rpc/shmem.h>
+#include "../../src/internal.h"
+
+static rpc_object_t
+exchange_blob(void *cookie, rpc_object_t args)
+{
+	rpc_shmem_block_t block;
+
+	if (rpc_object_unpack(args, "[h]", &block) != 0) {
+		rpc_function_error(cookie, EINVAL, "Invalid arguments passed");
+		return (NULL);
+	}
+
+	printf("Received %zu bytes long shared memory block\n",
+	    rpc_shmem_block_get_size(block));
+
+	memset(block->rsb_addr, 'B', block->rsb_size);
+	return (rpc_shmem_create(block));
+}
+
+int
+main(int argc, const char *argv[])
+{
+	rpc_context_t ctx;
+	rpc_server_t srv;
+
+	(void)argc;
+	(void)argv;
+
+	ctx = rpc_context_create();
+	rpc_context_register_func(ctx, "exchange_blob", "Exchanges binary blob",
+	    NULL, &exchange_blob);
+
+	rpc_discovery_register(ctx);
+	srv = rpc_server_create("unix:///tmp/server.sock", ctx);
+	pause();
+}
