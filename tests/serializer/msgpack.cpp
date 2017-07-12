ee1f0ffb06baebe8dde9b93f90939848fe9b9fc3
Wojciech Kloska <shangteus@gmail.com>
2017-07-12 10:53:03 -0700

    ADDED: msgpack serializer tests

--- tests/serializer/msgpack.cpp
+++ tests/serializer/msgpack.cpp
@@ -26,3 +26,186 @@
  */
 
 #include "../catch.hpp"
+
+#include <glib.h>
+#include <string.h>
+#include <unistd.h>
+#include <rpc/object.h>
+#include "../../src/serializer/msgpack.h"
+#include "../../src/serializer/json.h"
+#include "../../src/internal.h"
+
+static char json_dict_gold[] = "{"
+    "\"null\":null,"
+    "\"int\":-123,"
+    "\"uint\":{\"$uint\":123},"
+    "\"date\":{\"$date\":456},"
+    "\"binary\":{\"bin\":\"MTIzNA==\"},"
+    "\"fd\":{\"$fd\":1},"
+    "\"double\":12.0,"
+    "\"string\":\"deadbeef\","
+    "\"array\":[\"woopwoop\",-1234,{\"$fd\":2}]"
+    "}";
+
+static char json_array_gold[] = "["
+    "null,"
+    "-123,"
+    "{\"$uint\":123},"
+    "{\"$date\":456},"
+    "{\"bin\":\"MTIzNA==\"},"
+    "{\"$fd\":1},"
+    "12.0,"
+    "\"deadbeef\","
+    "{\"string\":\"woopwoop\",\"int\":-1234,\"fd\":{\"$fd\":2}}"
+    "]";
+
+static char json_single_gold[] = "\"asd\"";
+
+static char json_single_ext_gold[] = "{\"$uint\":123}";
+
+SCENARIO("MPACK_DICT_TEST", "Deserialize golden reference, serialize it again, and compare result") {
+	GIVEN("MPACK object") {
+		rpc_object_t object = NULL;
+		rpc_object_t object_mirror = NULL;
+		size_t buf_size;
+		void *buf = NULL;
+
+		object = rpc_json_deserialize((void *)json_dict_gold, strlen(json_dict_gold));
+
+		WHEN("RPC object is serialized") {
+			rpc_msgpack_serialize(object, &buf, &buf_size);
+
+			AND_WHEN("Output buffer is deserialized again") {
+				object_mirror = rpc_msgpack_deserialize(
+				    buf, buf_size);
+
+				REQUIRE(object_mirror != NULL);
+
+				THEN("Both RPC object and its mirror retrieved from deserialized MPACK are equal") {
+					REQUIRE(rpc_equal(object, object_mirror));
+				}
+			}
+		}
+
+		g_free(buf);
+		rpc_release(object);
+		rpc_release(object_mirror);
+	}
+}
+
+SCENARIO("MPACK_ARRAY_TEST", "Deserialize golden reference, serialize it again, and compare result") {
+	GIVEN("MPACK object") {
+		rpc_object_t object = NULL;
+		rpc_object_t object_mirror = NULL;
+		size_t buf_size;
+		void *buf = NULL;
+
+		object = rpc_json_deserialize((void *)json_array_gold, strlen(json_array_gold));
+
+		WHEN("RPC object is serialized") {
+			rpc_msgpack_serialize(object, &buf, &buf_size);
+
+			AND_WHEN("Output buffer is deserialized again") {
+				object_mirror = rpc_msgpack_deserialize(
+				    buf, buf_size);
+
+				REQUIRE(object_mirror != NULL);
+
+				THEN("Both RPC object and its mirror retrieved from deserialized MPACK are equal") {
+					REQUIRE(rpc_equal(object, object_mirror));
+				}
+			}
+		}
+
+		g_free(buf);
+		rpc_release(object);
+		rpc_release(object_mirror);
+	}
+}
+
+SCENARIO("MPACK_SINGLE_TEST", "Deserialize golden reference, serialize it again, and compare result") {
+	GIVEN("MPACK object") {
+		rpc_object_t object = NULL;
+		rpc_object_t object_mirror = NULL;
+		size_t buf_size;
+		void *buf = NULL;
+
+		object = rpc_json_deserialize((void *)json_single_gold, strlen(json_single_gold));
+
+		WHEN("RPC object is serialized") {
+			rpc_msgpack_serialize(object, &buf, &buf_size);
+
+			AND_WHEN("Output buffer is deserialized again") {
+				object_mirror = rpc_msgpack_deserialize(
+				    buf, buf_size);
+
+				REQUIRE(object_mirror != NULL);
+
+				THEN("Both RPC object and its mirror retrieved from deserialized MPACK are equal") {
+					REQUIRE(rpc_equal(object, object_mirror));
+				}
+			}
+		}
+
+		g_free(buf);
+		rpc_release(object);
+		rpc_release(object_mirror);
+	}
+}
+
+SCENARIO("MPACK_SINGLE_EXT_TEST", "Deserialize golden reference, serialize it again, and compare result") {
+	GIVEN("MPACK object") {
+		rpc_object_t object = NULL;
+		rpc_object_t object_mirror = NULL;
+		size_t buf_size;
+		void *buf = NULL;
+
+		object = rpc_json_deserialize((void *)json_single_ext_gold, strlen(json_single_ext_gold));
+
+		WHEN("RPC object is serialized") {
+			rpc_msgpack_serialize(object, &buf, &buf_size);
+
+			AND_WHEN("Output buffer is deserialized again") {
+				object_mirror = rpc_msgpack_deserialize(
+				    buf, buf_size);
+
+				REQUIRE(object_mirror != NULL);
+
+				THEN("Both RPC object and its mirror retrieved from deserialized MPACK are equal") {
+					REQUIRE(rpc_equal(object, object_mirror));
+				}
+			}
+		}
+
+		g_free(buf);
+		rpc_release(object);
+		rpc_release(object_mirror);
+	}
+}
+
+#if defined(__linux__)
+SCENARIO("MPACK_SHMEM_TEST", "Serialize and deserialize an RPC object representing a chunk of shared memory") {
+	GIVEN("Chunk of shared memory") {
+		rpc_object_t shmem = rpc_shmem_create(1024);
+		rpc_object_t parent = rpc_dictionary_create();
+		rpc_object_t mirror = NULL;
+		size_t buf_size;
+		void *buf = NULL;
+
+		rpc_dictionary_steal_value(parent, "mem", shmem);
+
+		WHEN("RPC object is serialized and deserialized again") {
+			rpc_msgpack_serialize(parent, &buf, &buf_size);
+			mirror = rpc_msgpack_deserialize(buf, buf_size);
+
+			THEN("Both source and result are the same") {
+				REQUIRE(rpc_equal(parent, mirror));
+			}
+		}
+
+		rpc_release(parent);
+		rpc_release(mirror);
+		g_free(buf);
+	}
+}
+#endif
