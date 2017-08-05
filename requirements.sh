c0a9e47769f6de891d1c032d6d9ab2213ef47897
Dennis Gentry <dennis.gentry@gmail.com>
2017-08-05 13:42:22 -0700

    Cases for linux and Mac OS.

---
+++ requirements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case $OSTYPE in
+    darwin*)
+        echo "I am a Mac.  Brew installing stuff."
+        echo "Don't worry about warnings of things already installed."
+        brew install --upgrade glib
+        brew install libsoup yajl
+        brew install pygobject3 --with-python3 gtk+3 ;;
+    *linux*)
+        echo "I am some kind of linux."
+        sudo apt install cmake clang libglib2.0-dev libsoup-gnome2.4-dev libyajl-dev libblocksruntime-dev ;;
+    default)
+        echo "Don't know how to set up for your OS."
+        exit 1
+esac
