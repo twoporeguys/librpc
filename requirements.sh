b1ae0c5b3f0f672158a170450f0105535aef361c
Dennis Gentry <dennis.gentry@gmail.com>
2017-08-05 13:47:45 -0700

    pygobject and gtk3 should get installed from the project that actually needs them.

--- requirements.sh
+++ requirements.sh
@@ -6,10 +6,11 @@ case $OSTYPE in
         echo "Don't worry about warnings of things already installed."
         brew install --upgrade glib
         brew install libsoup yajl
-        brew install pygobject3 --with-python3 gtk+3 ;;
+        ;;
     *linux*)
         echo "I am some kind of linux."
-        sudo apt install cmake clang libglib2.0-dev libsoup-gnome2.4-dev libyajl-dev libblocksruntime-dev ;;
+        sudo apt install cmake clang libglib2.0-dev libsoup-gnome2.4-dev libyajl-dev libblocksruntime-dev
+        ;;
     default)
         echo "Don't know how to set up for your OS."
         exit 1
