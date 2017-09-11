9f894fc77f807b36cb1484f3b77e5f35bb2d7de4
Travis CI <travis@travis-ci.org>
2017-09-11 17:45:07 +0000

    Deploy code docs to GitHub Pages (build #139)
    Commit: 597b535af24187e38d6b7095ea827e8cdb1c0c64

---
+++ html/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "bcdors",
+  1: "r",
+  2: "bcdos",
+  3: "r",
+  4: "r",
+  5: "r",
+  6: "r",
+  7: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "typedefs",
+  5: "enums",
+  6: "enumvalues",
+  7: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Typedefs",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Macros"
+};
+
--- html/search/typedefs_0.js
+++ html/search/typedefs_0.js
@@ -1,19 +1,19 @@
 var searchData=
 [
-  ['rpc_5farray_5fapplier_5ft',['rpc_array_applier_t',['../object_8h.html#afbd36072a4116c378f0ce243173251d1',1,'object.h']]],
-  ['rpc_5farray_5fcmp_5ft',['rpc_array_cmp_t',['../object_8h.html#a23bdaa1b433c1ae44515a60f06a86866',1,'object.h']]],
+  ['rpc_5farray_5fapplier_5ft',['rpc_array_applier_t',['../object_8h.html#ac685a2916978204cdac2bc8c855f7b00',1,'object.h']]],
+  ['rpc_5farray_5fcmp_5ft',['rpc_array_cmp_t',['../object_8h.html#a37160a10fb9baee827063f3db977315d',1,'object.h']]],
   ['rpc_5fcall_5fstatus_5ft',['rpc_call_status_t',['../connection_8h.html#a5e44285dc90be1f6bc541504a3f6d929',1,'connection.h']]],
   ['rpc_5fcall_5ft',['rpc_call_t',['../connection_8h.html#aeb6f25e395cc04930537960ef6b50106',1,'connection.h']]],
-  ['rpc_5fcallback_5ft',['rpc_callback_t',['../connection_8h.html#adc95de7fabf9904e1be453f5c6392a6b',1,'connection.h']]],
+  ['rpc_5fcallback_5ft',['rpc_callback_t',['../connection_8h.html#a659dfc386fa0add62a07c17662a29641',1,'connection.h']]],
   ['rpc_5fconnection_5ft',['rpc_connection_t',['../connection_8h.html#a70838cb106c3464db299522c5fe2782d',1,'connection.h']]],
   ['rpc_5fcontext_5ft',['rpc_context_t',['../service_8h.html#a55087f28cb63ff45b6c798a5dabaedd7',1,'service.h']]],
-  ['rpc_5fdictionary_5fapplier_5ft',['rpc_dictionary_applier_t',['../object_8h.html#a7d7c845c8ab67dc47ec85d89b7dbee2d',1,'object.h']]],
+  ['rpc_5fdictionary_5fapplier_5ft',['rpc_dictionary_applier_t',['../object_8h.html#affae185751cc133a978a305d97cd0c5a',1,'object.h']]],
   ['rpc_5ferror_5fcode_5ft',['rpc_error_code_t',['../connection_8h.html#a043170f29cb25827a88be4dab2313a91',1,'connection.h']]],
-  ['rpc_5ferror_5fhandler_5ft',['rpc_error_handler_t',['../connection_8h.html#a8dee2cbce5bf6bbb8de1d4bd95936a4a',1,'connection.h']]],
-  ['rpc_5ffunction_5ff',['rpc_function_f',['../service_8h.html#a9aa88314a3b4620186bf3142798a708c',1,'service.h']]],
-  ['rpc_5ffunction_5ft',['rpc_function_t',['../service_8h.html#aacbe55ace3fb9f574028b8d4ed28bb70',1,'service.h']]],
-  ['rpc_5fhandler_5ft',['rpc_handler_t',['../connection_8h.html#a5b4b4d2a4e3a7585ea8fe7c25256cf9d',1,'connection.h']]],
+  ['rpc_5ferror_5fhandler_5ft',['rpc_error_handler_t',['../connection_8h.html#a4dfba30ff97d4c5c56a6787ecd1bb79a',1,'connection.h']]],
+  ['rpc_5ffunction_5ff',['rpc_function_f',['../service_8h.html#a1f67d69a108ffc8087346616d82eacf7',1,'service.h']]],
+  ['rpc_5ffunction_5ft',['rpc_function_t',['../service_8h.html#a02d3dbd723de9bd5140887c9935ff05a',1,'service.h']]],
+  ['rpc_5fhandler_5ft',['rpc_handler_t',['../connection_8h.html#a65fe4b58831bfa2c2aa2210a48c52af2',1,'connection.h']]],
   ['rpc_5fobject_5ft',['rpc_object_t',['../object_8h.html#ab365f726b4975c0c8376b808d111d01b',1,'object.h']]],
-  ['rpc_5fserver_5fevent_5fhandler_5ft',['rpc_server_event_handler_t',['../server_8h.html#a3b3e718b54bebba70da20b9a26c9af9f',1,'server.h']]],
+  ['rpc_5fserver_5fevent_5fhandler_5ft',['rpc_server_event_handler_t',['../server_8h.html#af1a4a4bd63cb415ee40b34cec2df2a80',1,'server.h']]],
   ['rpc_5fserver_5ft',['rpc_server_t',['../server_8h.html#a46fd27cbcf75a54103df9d51d90a7fca',1,'server.h']]]
 ];
