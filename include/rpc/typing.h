d4be0d1749fd8b901050921cb41c2f171dafa197
Jakub Klama <jakub.klama@twoporeguys.com>
2017-09-16 22:01:16 +0200

    Fix tpyo.

--- include/rpc/typing.h
+++ include/rpc/typing.h
@@ -79,7 +79,7 @@ const char *rpct_type_get_realm(rpct_type_t type);
 const char *rpct_type_get_description(rpct_type_t type);
 rpct_type_t rpct_type_get_parent(rpct_type_t type);
 int rpct_type_get_generic_vars_count(rpct_type_t type);
-rpct_type_t rpct_typei_get_tpye(rpct_typei_t typei);
+rpct_type_t rpct_typei_get_type(rpct_typei_t typei);
 rpct_typei_t rpct_typei_get_generic_var(rpct_typei_t typei, int index);
 const char *rpct_typei_get_canonical_form(rpct_typei_t typei);
 
