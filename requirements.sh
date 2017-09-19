74dc6dd4ccf482ec2ea6450315a1a7d808d876d8
Jakub Klama <jakub.klama@gmail.com>
2017-09-20 01:28:21 +0200

    Squeeze all the brew deps into one line

--- requirements.sh
+++ requirements.sh
@@ -4,8 +4,7 @@ case $OSTYPE in
     darwin*)
         echo "I am a Mac.  Brew installing stuff."
         echo "Don't worry about warnings of things already installed."
-        brew install --upgrade glib
-        brew install libsoup yajl libusb libyaml python3
+        brew install --upgrade glib libsoup yajl libusb libyaml python3
         pip3 install cython
         ;;
     *linux*)
