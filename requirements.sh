649ba1c9d57b185a552a2c8ae29e5252708689da
Jakub Klama <jakub.klama@gmail.com>
2017-09-20 02:00:22 +0200

    Add yajl to requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,7 @@ case $OSTYPE in
     darwin*)
         echo "I am a Mac.  Brew installing stuff."
         echo "Don't worry about warnings of things already installed."
-        brew install glib libsoup yajl libusb libyaml python3
+        brew install glib libsoup yajl libusb libyaml yajl python3
         pip3 install cython
         ;;
     *linux*)
