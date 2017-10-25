9c22e066e0183a771e2ee02495bf7d2efd7f88cc
Travis CI <travis@travis-ci.org>
2017-10-25 04:08:38 +0000

    Deploy code docs to GitHub Pages (build #263)
    Commit: 1f5dc525f7b938906ee71fdd1bede91d8b7f4e27

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -96,6 +96,7 @@ var searchData=
   ['rpc_5fcopy',['rpc_copy',['../object_8h.html#aa5de9854b486016a16ac230ae0ba1d3a',1,'object.h']]],
   ['rpc_5fcopy_5fdescription',['rpc_copy_description',['../object_8h.html#a8687bd8dcf67458dbb09549d2a9b6059',1,'object.h']]],
   ['rpc_5fdata_5fcreate',['rpc_data_create',['../object_8h.html#af82b4d55631d1b9c59bd10b6939fac52',1,'object.h']]],
+  ['rpc_5fdata_5fcreate_5fiov',['rpc_data_create_iov',['../object_8h.html#a78262ca3dea42ec4757c1ec2a3db9eb1',1,'object.h']]],
   ['rpc_5fdata_5fget_5fbytes',['rpc_data_get_bytes',['../object_8h.html#a0453cb25c7fbeffd9151bfd8960c1656',1,'object.h']]],
   ['rpc_5fdata_5fget_5fbytes_5fptr',['rpc_data_get_bytes_ptr',['../object_8h.html#a626c045e08849189e2d75f191ce6c903',1,'object.h']]],
   ['rpc_5fdata_5fget_5flength',['rpc_data_get_length',['../object_8h.html#a17b4b5077bb28dad2b678e64001c6824',1,'object.h']]],
