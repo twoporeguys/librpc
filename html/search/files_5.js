1a8a3fb963b2a0a354daa64c45c8206341f4a1b8
Travis CI <travis@travis-ci.org>
2017-11-27 22:20:07 +0000

    Deploy code docs to GitHub Pages (build #307)
    Commit: b733f3b1b8fc8b1153867e094562c8588f8fc6fe

--- html/search/files_5.js
+++ html/search/files_5.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['serializer_2eh',['serializer.h',['../serializer_8h.html',1,'']]],
-  ['server_2eh',['server.h',['../server_8h.html',1,'']]],
-  ['service_2eh',['service.h',['../service_8h.html',1,'']]]
+  ['typing_2eh',['typing.h',['../typing_8h.html',1,'']]]
 ];
