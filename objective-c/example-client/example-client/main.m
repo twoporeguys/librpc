25d26bef5a399c75709c65bb0c6c57902445cab1
Jakub Klama <jakub.klama@twoporeguys.com>
2017-12-21 22:38:23 +0100

    Add minimal working callSync and callAsync in Obj-C and two example programs (Obj-C and Swift).

---
+++ objective-c/example-client/example-client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  example-client
+//
+//  Created by Jakub Klama on 19.12.2017.
+//  Copyright © 2017 Jakub Klama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <librpc.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ objective-c/example-swift/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  example-swift
+//
+//  Created by Jakub Klama on 19.12.2017.
+//  Copyright © 2017 Jakub Klama. All rights reserved.
+//
+
+import Foundation
+import librpc
+
+var client = librpc.RPCClient();
+client.connect("tcp://10.251.2.127:5001");
+var result = client.callSync("ping", path: "/server", interface: "com.twoporeguys.momd.Builtin", args: nil);
+
+client.callAsync("ping", path: "/server", interface: "com.twoporeguys.momd.Builtin", args: nil, callback: { (call: librpc.RPCCall?, value: librpc.RPCObject?) in
+    print("async result", value?.describe());
+});
+sleep(2);
+print(result?.describe());
+var foo: UInt32 = 0xffffccff;
+var a = librpc.RPCObject.initWithValue(["A":5, "b":true, "c":3.5, "d":foo]);
+print(a?.describe());
+
