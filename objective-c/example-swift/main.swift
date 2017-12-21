7434dec3cbc77122e0f2f9d8690e7c3b8c82b52d
Jakub Klama <jakub.klama@twoporeguys.com>
2017-12-21 23:35:53 +0100

    Add nullability annotations.

--- objective-c/example-swift/main.swift
+++ objective-c/example-swift/main.swift
@@ -13,12 +13,12 @@ var client = librpc.RPCClient();
 client.connect("tcp://10.251.2.127:5001");
 var result = client.callSync("ping", path: "/server", interface: "com.twoporeguys.momd.Builtin", args: nil);
 
-client.callAsync("ping", path: "/server", interface: "com.twoporeguys.momd.Builtin", args: nil, callback: { (call: librpc.RPCCall?, value: librpc.RPCObject?) in
-    print("async result", value?.describe());
+client.callAsync("ping", path: "/server", interface: "com.twoporeguys.momd.Builtin", args: nil, callback: { (call: librpc.RPCCall, value: librpc.RPCObject) in
+    print("async result", value.describe());
 });
 sleep(2);
-print(result?.describe());
+print(result.describe());
 var foo: UInt32 = 0xffffccff;
 var a = librpc.RPCObject.initWithValue(["A":5, "b":true, "c":3.5, "d":foo]);
-print(a?.describe());
+print(a.describe());
 
--- objective-c/librpc/librpc.h
+++ objective-c/librpc/librpc.h
@@ -9,39 +9,39 @@
 #import <Foundation/Foundation.h>
 
 @interface RPCObject : NSObject
-+ (RPCObject *)initWithValue:(id)value;
-+ (RPCObject *)initFromNativeObject:(void *)object;
-- (NSString *)describe;
-- (NSObject *)value;
-- (void *)nativeValue;
++ (nonnull RPCObject *)initWithValue:(nullable id)value;
++ (nonnull RPCObject *)initFromNativeObject:(nullable void *)object;
+- (nonnull NSString *)describe;
+- (nonnull NSObject *)value;
+- (nonnull void *)nativeValue;
 @end
 
 @interface RPCCall : NSObject
-+ (RPCCall *)initFromNativeObject:(void *)object;
++ (nonnull RPCCall *)initFromNativeObject:(nonnull void *)object;
 - (void)wait;
 - (void)resume;
 - (void)abort;
-- (RPCObject *)result;
+- (nullable RPCObject *)result;
 @end
 
-typedef void(^RPCFunctionCallback)(RPCCall *call, RPCObject *value);
+typedef void(^RPCFunctionCallback)(RPCCall * _Nonnull call, RPCObject * _Nonnull value);
 
 @interface RPCClient : NSObject
-- (void)connect:(NSString *)uri;
+- (void)connect:(nonnull NSString *)uri;
 - (void)disconnect;
-- (NSDictionary *)instances;
-- (RPCCall *)call:(NSString *)method path:(NSString *)path interface:(NSString *)interface args:(RPCObject *)args;
-- (RPCObject *)callSync:(NSString *)method path:(NSString *)path interface:(NSString *)interface args:(RPCObject *)args;
-- (void)callAsync:(NSString *)method path:(NSString *)path interface:(NSString *)interface args:(RPCObject *)args callback:(RPCFunctionCallback)cb;
+- (nonnull NSDictionary *)instances;
+- (nonnull RPCCall *)call:(nonnull NSString *)method path:(nullable NSString *)path interface:(nullable NSString *)interface args:(nullable RPCObject *)args;
+- (nonnull RPCObject *)callSync:(nonnull NSString *)method path:(nullable NSString *)path interface:(nullable NSString *)interface args:(nullable RPCObject *)args;
+- (void)callAsync:(nonnull NSString *)method path:(nullable NSString *)path interface:(nullable NSString *)interface args:(nullable RPCObject *)args callback:(nonnull RPCFunctionCallback)cb;
 @end
 
 @interface RPCInstance : NSObject
-@property (readonly) RPCClient *client;
-@property (readonly) NSString *path;
+@property (readonly, nonnull) RPCClient *client;
+@property (readonly, nonnull) NSString *path;
 @end
 
 @interface RPCInterface : NSObject
-@property (readonly) RPCInstance *instance;
-- (void)forwardInvocation:(NSInvocation *)anInvocation;
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector;
+@property (readonly, nonnull) RPCInstance *instance;
+- (void)forwardInvocation:(nonnull NSInvocation *)anInvocation;
+- (nonnull NSMethodSignature *)methodSignatureForSelector:(nonnull SEL)aSelector;
 @end
