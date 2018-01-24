8f6de998ac20efb94171be2679df1d88e4fb3e7c
Jakub Klama <jakub.klama@twoporeguys.com>
2018-01-23 21:47:21 -0800

    Add initial Rust bindings.

---
+++ bindings/rust/src/test.rs
@@ -0,0 +1,12 @@
+fn test(a: u32) -> u32 {
+    'a {
+        let a = 5;
+        'b {
+            let b = 3;
+            'c {
+                let c = "test";
+                fn('a a, 'b b, 'c c);
+            }
+        }
+    }
+}
