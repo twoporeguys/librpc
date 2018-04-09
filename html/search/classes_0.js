d7f0a3957fdea6b4d9fdce85953977c09b6ef96e
Travis CI <travis@travis-ci.org>
2018-04-09 00:05:43 +0000

    Deploy code docs to GitHub Pages (build #736)
    Commit: da0ba617ad921a20aa6b7c657a8ed286b6ab5573

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['rpc_5fbus_5fnode',['rpc_bus_node',['../structrpc__bus__node.html',1,'']]],
+  ['rpc_5fconnection_5fstatistics',['rpc_connection_statistics',['../structrpc__connection__statistics.html',1,'']]],
   ['rpc_5fif_5fmember',['rpc_if_member',['../structrpc__if__member.html',1,'']]],
   ['rpc_5fif_5fmethod',['rpc_if_method',['../structrpc__if__method.html',1,'']]],
   ['rpc_5fif_5fproperty',['rpc_if_property',['../structrpc__if__property.html',1,'']]],
