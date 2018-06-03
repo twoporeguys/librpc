a931a348bd6c2231c2c1b0d68a6c95b822b2939f
Jakub Klama <jakub.klama@twoporeguys.com>
2018-06-03 02:50:04 +0200

    Add pps readings to benchmark client app.

--- examples/benchmark/client.c
+++ examples/benchmark/client.c
@@ -173,6 +173,7 @@ next:
 	printf("Received %" PRId64 " messages and %" PRId64 " bytes\n", cycles, bytes);
 	printf("It took %.04f seconds\n", elapsed);
 	printf("Average data rate: %.04f MB/s\n", bytes / elapsed / 1024 / 1024);
+	printf("Average packet rate: %.04f\n", cycles / elapsed);
 	return (EXIT_SUCCESS);
 
 error:
