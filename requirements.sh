9d5e43a53c97ac60af3594d9452d37c0338fb86f
Brett Gyarfas <bg@2pg.io>
2018-06-21 14:35:00 -0700

    Missing ubuntu 18.04 dependencies

--- requirements.sh
+++ requirements.sh
@@ -23,7 +23,7 @@ case "`uname -s`" in
             cmake clang libglib2.0-dev libsoup-gnome2.4-dev \
             libyajl-dev libblocksruntime-dev libyaml-dev libudev-dev \
             libusb-1.0-0-dev python3 cython3 python3-sphinx \
-	        python3-setuptools
+	        python3-setuptools libsystemd-dev libelf-dev
         ;;
     default)
         echo "Don't know how to set up for your OS."
